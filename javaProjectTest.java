1bb5af270b653986a60f194ba13c0a94be2547e7
LakshminarayanaTejas <78833968+LakshminarayanaTejas@users.noreply.github.com>
2021-03-04 23:07:36 -0500

    Add files via upload

---
+++ javaProjectTest.java
@@ -0,0 +1,14 @@
+package test.java.cases;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class javaProjectTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
